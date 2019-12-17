cf287f36d114702946501ba7872c8db38ee63161
munderseth <marku@s2technologies.com>
2019-12-17 14:24:22 -0800

    swith to open plan

--- actions/org/create/orgCreate.js
+++ actions/org/create/orgCreate.js
@@ -4,26 +4,31 @@ const core = require('@actions/core');
 async function run() {
     try {
 
+        console.log("Setup Testspace Org script starting ...")
         const tsOrg      = core.getInput('ts-org');
         const tsUser     = core.getInput('ts-username');
         const tsPassword = core.getInput('ts-password');
-
-        await openBrowser();
-       // await openBrowser({ headless: true, ignoreCertificateErrors: true, args:['--window-size=1024,900']})
+        
+       // await openBrowser();
+        await openBrowser({ headless: true, ignoreCertificateErrors: true, args:['--window-size=1024,900']})
         await goto("signin.stridespace.com");
         await write(tsUser);
         await press("Tab");
         await write(tsPassword);
         await click("SUBMIT");
-
-        await goto("signup.stridespace.com/?plan_id=basic-2");
+        console.log("Have logged in now ..")
+       // await goto("signup.stridespace.com/?plan_id=basic-2");
+        await goto("signup.stridespace.com/?plan_id=open_v1");
         await click("Sign up with Email");
         await write(tsOrg);
         await click("SUBMIT");
-        await write("4242 4242 4242 4242");
-        await press("Tab");
-        await write("123");
-        await click("COMPLETE ACCOUNT");
+        console.log("Waiting for new Complete")
+        waitFor(5000)
+        await click("Complete")
+       // await write("4242 4242 4242 4242");
+       // await press("Tab");
+       // await write("123");
+       // await click("COMPLETE ACCOUNT");
               
     } catch (error) {
         console.error(error);
