5ea8cee3164a250847190e641a83eea0ad673d62
munderseth <marku@s2technologies.com>
2019-12-16 16:53:55 -0800

    logs

--- actions/orgcreate/orgCreate.js
+++ actions/orgcreate/orgCreate.js
@@ -24,6 +24,7 @@ async function run() {
         await write("4242 4242 4242 4242");
         await press("Tab");
         await write("123");
+        console.log(tsOrg, tsUser)
         await click("COMPLETE ACCOUNT");
        
         await click("Account");
